49c1a07b629e205fe909a91aa4d97967dde72622
misa kohata <samisami1215@icloud.com>
2021-11-11 21:41:21 +0900

    フォーム内のコード書き換え、jQueryでの挙動をとれるようなコードに修正

--- app/javascript/packs/form.js
+++ app/javascript/packs/form.js
@@ -1,25 +1,28 @@
 
 
+//= require jquery
+//= require jquery_ujs
+
 // ---------------- クリック時の挙動 -----------------
 
-// hiddenタグを隠す（やや悪い・悪いクリック時のみ表示させる）
-document.getElementById("hidden").style.display = "none";
-
-//良い
-document.getElementById("daily_record_condition_good").addEventListener("click", function(){
-    document.getElementById("hidden").style.display = "none";
+// スライドダウン（やや悪い）
+$("#daily_record_condition_somewhat_bad,#daily_record_condition_bad").click(function(){
+    //トグル
+    $("#hidden").slideDown(1500);
 });
 
-//やや悪い
-document.getElementById("daily_record_condition_somewhat_bad").addEventListener("click", function(){
-    document.getElementById("hidden").style.display = "block";
-    
+$("#daily_record_condition_good").click(function(){
+    //トグル
+    $("#hidden").slideUp(1500);
 });
+
+
+
 
 //悪い
-document.getElementById("daily_record_condition_bad").addEventListener("click", function(){
-    document.getElementById("hidden").style.display = "block";
-});
+// document.getElementById("daily_record_condition_bad").addEventListener("click", function(){
+//     document.getElementById("hidden").style.display = "block";
+// });
 
 // document.getElementById("condition").onclick = function() {
 //     hidden.style.display = "block";
