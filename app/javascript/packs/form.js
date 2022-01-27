10c6cff7e1c36284bc043376b4dea37ab714fa20
misa kohata <samisami1215@icloud.com>
2022-01-28 00:00:39 +0900

    フォーム画面の挙動
    id名とclass名が変更した為、再度入力

--- app/javascript/packs/form.js
+++ app/javascript/packs/form.js
@@ -13,70 +13,70 @@ const { start } = require("@rails/activestorage");
 // ---------------- 体調ボタンのやや悪い・悪いクリック時の挙動 -----------------
 
 // スライドダウン
-$("#daily_record_condition_somewhat_bad,#daily_record_condition_bad").click(function(){
+$("#daily_record_note_condition_somewhat_bad,#daily_record_note_condition_bad").click(function(){
   //トグル
-  $("#hidden_detail").slideDown(1500);
+  $("#daily_hidden_detail").slideDown(1500);
 });
 // 良いをクリックするとスライドアップ
-$("#daily_record_condition_good").click(function(){
+$("#daily_record_note_condition_good").click(function(){
   //トグル
-  $("#hidden_detail").slideUp(1500);
+  $("#daily_hidden_detail").slideUp(1500);
 });
 
 // ---------------- 排便クリック時、有りの場合の挙動 ----------------------------------------------------------------
-$("#daily_record_defecation_yes").click(function(){
+$("#daily_record_note_defecation_yes").click(function(){
   //トグル
-  $("#stool_hidden").slideDown(1500);
+  $("#daily_stool_hidden").slideDown(1500);
 });
 // 無しをクリックするとスライドアップ
-$("#daily_record_defecation_none").click(function(){
+$("#daily_record_note_defecation_none").click(function(){
   //トグル
-  $("#stool_hidden").slideUp(1500);
+  $("#daily_stool_hidden").slideUp(1500);
 });
 
 // ---------------- 怪我クリック時、有りの場合の挙動 ----------------------------------------------------------------
-$("#daily_record_injury_yes").click(function(){
+$("#daily_record_note_injury_yes").click(function(){
   //トグル
-  $("#injury_hidden").slideDown(1500);
+  $("#daily_injury_hidden").slideDown(1500);
 });
 // 無しをクリックするとスライドアップ
-$("#daily_record_injury_none").click(function(){
+$("#daily_record_note_injury_none").click(function(){
   //トグル
-  $("#injury_hidden").slideUp(1500);
+  $("#daily_injury_hidden").slideUp(1500);
 });
 
 // ---------------- (朝食)残食ボタンクリック時の挙動 ----------------------------------
 // スライドダウン
-$("#daily_record_morning_snack_remaining").click(function(){
+$("#daily_record_note_morning_snack_remaining").click(function(){
   //トグル
   $("#morning_hidden").slideDown(1500);
 });
 // スライドアップ
-$("#daily_record_morning_snack_complete").click(function(){
+$("#daily_record_note_morning_snack_complete").click(function(){
   //トグル
   $("#morning_hidden").slideUp(1500);
 });
 
 // ---------------- (昼食)残食ボタンクリック時の挙動 ----------------------------------
 // スライドダウン
-$("#daily_record_noon_remaining").click(function(){
+$("#daily_record_note_noon_remaining").click(function(){
   //トグル
   $("#noon_hidden").slideDown(1500);
 });
 // スライドアップ
-$("#daily_record_noon_complete").click(function(){
+$("#daily_record_note_noon_complete").click(function(){
   //トグル
   $("#noon_hidden").slideUp(1500);
 });
 
 // ---------------- (午後おやつ)残食ボタンクリック時の挙動 ----------------------------------
 // スライドダウン
-$("#daily_record_noon_snack_remaining").click(function(){
+$("#daily_record_note_noon_snack_remaining").click(function(){
   //トグル
   $("#noon_snack_hidden").slideDown(1500);
 });
 // スライドアップ
-$("#daily_record_noon_snack_complete").click(function(){
+$("#daily_record_note_noon_snack_complete").click(function(){
   //トグル
   $("#noon_snack_hidden").slideUp(1500);
 });
