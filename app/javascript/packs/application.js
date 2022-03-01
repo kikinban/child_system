7c5883322f1d930b1fb05ad8e4f6ff2f4519e64e
misa kohata <samisami1215@icloud.com>
2022-03-01 23:02:54 +0900

    jQueryを追加
    サーバーにアップデートするとエラーになる。ローカルは出ない

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+//= require jquery
+//= require jquery.turbolinks
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
 
