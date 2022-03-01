fc9052ab8240cff5fc2856a545f19713c77eb091
misa kohata <samisami1215@icloud.com>
2022-03-01 23:34:16 +0900

    jquery本番読み込まれない　修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
-//= require jquery
-//= require jquery.turbolinks
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
-
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require('jquery')
